3e55931d8e37ccbd8b79831596674e3ec4d6103b
OluwasayoAwoyemi <danielawoyemi108@outlook.com>
2025-02-23 01:38:48 -0600

    Almost fucked up

--- serenity/src/Components/Chat.js
+++ serenity/src/Components/Chat.js
@@ -29,19 +29,6 @@ function Chat() {
 
   return (
     <>
-<<<<<<< Updated upstream
-  <div className="chat">
-    <h1 className="chat-title dancing-script">Serenity Chat 🌿</h1> {/* Title with Dancing Script font */}
-    <SignOut />
-    <div className="messages">
-      {messages.map((message) => (
-        <div
-          key={message.id}
-          className={`msg ${message.uid === auth.currentUser.uid ? 'sent' : 'received'}`}
-        >
-          {message.photoURL && <img src={message.photoURL} alt="User Avatar" />}
-          <p>{message.text}</p> 
-=======
       <div className="chat">
         <h1 className="chat-title dancing-script">Chat 🌿</h1> {/* Title with Dancing Script font */}
         <SignOut />
@@ -57,21 +44,7 @@ function Chat() {
           ))}
           {/* Invisible div for smooth scrolling */}
           <div ref={messagesEndRef} />
->>>>>>> Stashed changes
         </div>
-<<<<<<< Updated upstream
-      ))}
-      {/* Invisible div for smooth scrolling */}
-      <div ref={messagesEndRef} />
-    </div>
-    <SendMessage auth={auth} />  
-  </div>
-      <div className="small-box" />
-      <div> <button class="talk-ai-button"> Talk To Serenity AI</button></div>
-  </>
-
-);
-=======
         <SendMessage auth={auth} />
       </div>
       
@@ -87,7 +60,6 @@ function Chat() {
 
     </>
   );
->>>>>>> Stashed changes
 }
 
 export default Chat;
